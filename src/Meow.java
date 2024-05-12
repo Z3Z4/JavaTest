c6f37125a65b528054240b95b1a7e73db406f737
Z3Z4 <89277110+Z3Z4@users.noreply.github.com>
2024-05-12 18:31:10 +0300

    Delete src directory

--- src/Meow.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Meow {
-    public static void main(String[] args) {
-        System.out.println("What is your name?");
-        String x = new Scanner(System.in).nextLine();
-        System.out.println("Your Name is "+x);
-    }
-}
