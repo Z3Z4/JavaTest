8d625f84bd73c993250562132bf28213c31679b9
Z3Z4 <vladislav.rusin@gmail.com>
2024-06-17 13:50:48 +0300

    Создал несколько двумерных массивов, validArray, invalidSizeArray, invalidDataArray, обработал исключения в блоке try-catch, описал логику изменения двумерного массива в changeMatrix, так же создал метод для отображения двумерного массива в методе showMatrix

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,72 @@
-import animals.*;
-import task2.*;
+import exception.MyArrayDataException;
+import exception.MyArraySizeException;
 
 public class Main {
     public static void main(String[] args) {
+        String[][] validArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16",}
+        };
 
-        Cat cats[] = new Cat[5];
-        for (int i = 0; i < cats.length; i++) {
-            cats[i] = new Cat("Kotya" + i);
-            System.out.println(cats[i].getName() + " Появился на свет");
+        String[][] invalidSizeArray = {
+                {"1", "2", "3"},
+                {"4", "5", "6"},
+                {"7", "8", "9"}
+        };
+
+        String[][] invalidDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "meow", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+        try {
+            System.out.println(changeMatrix(validArray));
+        } catch (MyArraySizeException ex) {
+            ex.printStackTrace();
+        } catch (MyArrayDataException ex) {
+            ex.printStackTrace();
         }
-        Cat.addFoodToBowl(100);
-        Cat.getBowlOfFoodInfo();
-        for (Cat cat : cats) {
-            cat.feedCat(25);
-            System.out.println(cat.getName() + " наелся? " + cat.getFullness());
+
+
+    }
+
+
+    public static int changeMatrix(String[][] matrix) throws MyArraySizeException, MyArrayDataException {
+        if (matrix.length != 4) {
+            throw new MyArraySizeException("Matrix must be 4x4 size only");
         }
+        for (int g = 0; g < matrix.length; g++) {
+            if (matrix[g].length != 4) {
+                throw new MyArraySizeException("Array must be 4x4 in size.");
+            }
+        }
+        int result = 0;
 
-        System.out.println("");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                try {
+                    result += Integer.parseInt(matrix[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Invalid data at (" + i + ", " + j + "): " + matrix[i][j]);
+                }
+            }
+        }
+        return result;
+    }
 
-        cats[3].toSwim(5);
-        cats[3].toRun(250);
-
-        System.out.println("");
-
-//
-//        Dog dogs[] = new Dog[5];
-//        for (int i = 0; i < dogs.length; i++) {
-//            dogs[i] = new Dog("Bobik" + i);
-//            System.out.println(dogs[i].getName() + " Появился на свет");
-//        }
-//        Dog.addFoodToBowl(100);
-//        Dog.getBowlOfFoodInfo();
-//        for (int i = 0; i < dogs.length; i++) {
-//            dogs[i].feedDog(30);
-//            System.out.println(dogs[i].getName()+" наелся? "+dogs[i].getFullness());
-//        }
-//
-//        System.out.println("");
-
-        Animal.showAnimalCount();
-        Cat.showCatCount();
-        Dog.showDogCount();
-
-        System.out.println("");
-
-        Circle circle = new Circle(7);
-        circle.setFillColor("Жёлтый");
-        circle.setBorderColor("Черный");
-
-        Rectangle rectangle = new Rectangle(6, 13);
-        rectangle.setFillColor("Фиолетовый");
-        rectangle.setBorderColor("Красный");
-
-        Triangle triangle = new Triangle(3, 4, 6);
-        triangle.setFillColor("Синий");
-        triangle.setBorderColor("Белый");
-
-        System.out.println("Круг: Периметр = " + circle.getPerimeter() + ", Площадь = " + circle.getArea() + ", Цвет фона = " + circle.getFillColor() + ", Цвет границы = " + circle.getBorderColor());
-        System.out.println("Прямоугольник: Периметр = " + rectangle.getPerimeter() + ", Площадь = " + rectangle.getArea() + ", Цвет фона = " + rectangle.getFillColor() + ", Цвет границы = " + rectangle.getBorderColor());
-        System.out.println("Треугольник: Периметр = " + triangle.getPerimeter() + ", Площадь = " + triangle.getArea() + ", Цвет фона = " + triangle.getFillColor() + ", Цвет границы = " + triangle.getBorderColor());
+    public static void showMatrix(String[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
+
+
+
---
+++ src/exception/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException(String message){
+        super(message);
+
+    }
+}
