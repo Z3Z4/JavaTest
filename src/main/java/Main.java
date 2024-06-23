38facb5c4acf805639f76fdd4f71578829abef04
Z3Z4 <vladislav.rusin@gmail.com>
2024-06-23 13:40:21 +0300

    main

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+import com.FactorialCalculator.FactorialCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число для вычисления его факториала: ");
+        int number = scanner.nextInt();
+        long result = FactorialCalculator.factorial(number);
+        System.out.println("Факториал числа " + number + " равен " + result);
+    }
+
+}
+
+
+
+
+
+
